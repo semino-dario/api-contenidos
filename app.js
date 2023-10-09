9dbce62e228f74aaf355daafb1f2af5bf5c2f126
Dario Semino <orbislibroscontacto@gmail.com>
2023-10-08 23:14:13 -0300

    con netlify habilitado en cors

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(limiter);
 
 //Setup cors // Enable access by other domains
 const corsOptions = {
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://taupe-croquembouche-40c119.netlify.app'],
     optionsSuccessStatus: 200,
 };
 
