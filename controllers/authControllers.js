0acaf1b1ad174b4d8c089df6c83731b1ef8f03d1
Dario Semino <orbislibroscontacto@gmail.com>
2023-10-11 16:47:31 -0300

    index.html con documentación

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -84,7 +84,9 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
     // Create reset password url 
 
-    const resetUrl = `${process.env.FRONT_URL}/new-password/${resetToken}`;
+    //const resetUrl = `${process.env.FRONT_URL}/new-password/${resetToken}`;
+    const resetUrl = `http://localhost:3001/new-password/${resetToken}`;
+
     //const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
 
     const message = `Ingresar al siguiente link para recuperar la contraseña: \n\n${resetUrl}\n\n`
